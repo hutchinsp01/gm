db1e1cc6cde80569b1ca2e377c844e0dbd598ace
Paul Hutchins <paul.hutchins@uptickhq.com>
2024-12-10 22:45:52 +1100

    meme on leigh

---
+++ g.py
@@ -0,0 +1,9 @@
+def w(c,d,p=0):
+ v=[]
+ while len(v)<7e4:
+  v+=[c];x,y=d;n=i,j=c[0]+x,c[1]+y
+  if(0<=i<len(g)>j>=0)^1:return{*v}
+  c,d=((n,d),(c,(-y,x)))[g[j][i]=="#"or n==p]
+g=[*open(0)];i,_=d=s=0,-1
+for x in g:s=max(s,(x.find("^"),i));i+=1
+print(len(v:=w(s,d)),sum(bool(w(s,d,p))^1for p in v-{s}))
